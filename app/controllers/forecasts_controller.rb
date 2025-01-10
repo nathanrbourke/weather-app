e75b9e9aa48d03248ab8d449614b82d1d797eb9f
Nathan Bourke <nathanrbourke@gmail.com>
2025-01-10 08:46:13 -0500

    consolidate routes for now

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,13 +1,10 @@
 class ForecastsController < ApplicationController
   def index
-  end
-
-  def create
-    weather_reporting = WeatherReporting.new({ postal_code: params[:postal_code] })
-    @forecast = weather_reporting.daily_forecast
-    @current_weather = weather_reporting.current_weather
-    puts @forecast
-    puts @current_weather
-    render :index
+    if params[:postal_code]
+      weather_reporting = WeatherReporting.new({ postal_code: params[:postal_code] })
+      @daily_forecast = weather_reporting.daily_forecast
+      @current_weather = weather_reporting.current_weather
+      @last_cached_minutes_ago = 30
+    end
   end
 end
