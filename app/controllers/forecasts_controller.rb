2b86b780d2fcf128c68c8316f1fb3215d9174e4f
Nathan Bourke <nathanrbourke@gmail.com>
2025-01-11 08:38:14 -0500

    build proper form and RESTful routes

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,11 +1,16 @@
 class ForecastsController < ApplicationController
-  def index
-    if params[:postal_code]
-      daily_forecast_service = ::Service::DailyForecast.new({ postal_code: params[:postal_code] })
-      current_weather_service = ::Service::CurrentWeather.new({ postal_code: params[:postal_code] })
+  def new
+  end
 
-      @daily_forecast = daily_forecast_service.daily_forecast
-      @current_weather = current_weather_service.current_weather
-    end
+  def create
+    redirect_to forecast_path(params[:postal_code])
+  end
+
+  def show
+    daily_forecast_service = ::Service::DailyForecast.new({ postal_code: '06074' })
+    current_weather_service = ::Service::CurrentWeather.new({ postal_code: '06074' })
+
+    @daily_forecast = daily_forecast_service.daily_forecast
+    @current_weather = current_weather_service.current_weather
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "forecasts#index"
+  root 'forecasts#new'
 
-  resources :forecasts, only: [:index]
+  resources :forecasts, only: %i[new create show]
 end
